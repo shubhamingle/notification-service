adfba1f4c64fa229d2a74d3eb36039840788cfca
Shubham Ingle <37982804+shubhamingle@users.noreply.github.com>
2022-01-27 19:52:58 +0530

    Update Notification.java

--- src/main/java/com/reliance/notification/model/Notification.java
+++ src/main/java/com/reliance/notification/model/Notification.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Notification {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String message;
 
